9cc695b8c6785b65d0df80551899ce14a373c53e
Evan <evanhowington@yahoo.com>
2018-08-04 20:58:37 -0500

    completed the importing data into Python Part 2 with DataCamp
    This was not as productive as i would have liked for it to be due to not having access to various SSL Certicates for the websites that DataCamp imports its data from.

--- importing_data_from_the_internet.py
+++ importing_data_from_the_internet.py
@@ -1,41 +1,144 @@
+
+
+# import packages
+from urllib.request import urlopen, Request
+# Specify the url
+url = "http://www.google.com"
+# this packages the request: request
+request = Request(url)
+# this packages the request and catches the response
+response = urlopen(request)
+# print the datatype of response
+print(type(response))
+# Be polite and close the response!
+response.close()
+
 """
-# Import package
-from urllib.request import urlretrieve
+Parsing HTML with BeautifulSoup
+"""
 
-# Import pandas
-import pandas as pd
+# Import packages
+import requests
+from bs4 import BeautifulSoup
 
-# Assign url of file: url
-url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+# specify url: url
+url = 'https://www.python.org/~guido/'
+# package the request, send the request and cath the response: r
+r = requests.get(url)
+# extracts the response as html: html_doc
+html_doc = r.text
+# create a beatifulsoup object from the html: soup
+soup = BeautifulSoup(html_doc)
+# prettify the beautiful soup object: pretty_soup
+pretty_soup = soup.prettify()
+# print the response
+print(pretty_soup)
 
-# Save file locally
-urlretrieve(url, "winequality-red.csv")
+# Import packages
+import requests
+from bs4 import BeautifulSoup
 
+# Specify url: url
+url = 'https://www.python.org/~guido/'
 
-# Read file into a DataFrame and print its head
-df = pd.read_csv('winequality-red.csv', sep=';')
-print(df.head())
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
 
+# Extract the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Get the title of Guido's webpage: guido_title
+guido_title = soup.title
+
+# Print the title of Guido's webpage to the shell
+print(guido_title)
+
+# Get Guido's text: guido_text
+guido_text = soup.text
+
+# Print Guido's text to the shell
+print(guido_text)
 
 
 # Import packages
-import matplotlib.pyplot as plt
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-# Assign url of file: url
-url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+# Specify url
+url = 'https://www.python.org/~guido/'
 
-# Read file into a DataFrame: df
-df = pd.read_csv(url, sep = ';')
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
 
-# Print the head of the DataFrame
-print(df.head())
+# Extracts the response as html: html_doc
+html_doc = r.text
 
-# Plot first column of df
-pd.DataFrame.hist(df.ix[:, 0:1])
-plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
-plt.ylabel('count')
-plt.show()
+# create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc, features="html.parser")
+
+# Print the title of Guido's webpage
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all('a')
+
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get('href'))
+"""
+API requests
 """
 
-# ^^^^^ THE CODE ABOVE WORKS IN DATACAMP BUT DOES NOT WORK IN PYCHARM BECAUSE I DO NOT HAVE THE ABILITY TO VERIFY THE SSL
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=the+social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
+"""
+JSON from the Web to Python
+"""
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'http://www.omdbapi.com/?apikey=ff21610b&t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+"""
+Checking out the wikipedia api
+"""
+
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
